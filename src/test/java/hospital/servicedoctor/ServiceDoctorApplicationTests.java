d4bd2412cadd67cc08ea14b35dff329479630a72
stefanozogno <1001900@stud.unive.it>
2024-11-30 12:54:43 +0100

    Initialize SpringBoot project with starter dependencies

---
+++ src/test/java/hospital/servicedoctor/ServiceDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.servicedoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
